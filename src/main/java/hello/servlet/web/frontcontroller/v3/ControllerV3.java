2479626869f81af6eab56e6056d4ad8dd589692a
dahae0405 <dahae0405@naver.com>
2021-10-18 16:44:06 +0900

    프론트 컨트롤러 v3,4,5 생성
    프론트컨트롤러 각각 v3,v4(모델통합)버전 생성
    프론트컨트롤로 v5(v3,v4컨트롤러 통합) 버전 생성

--- src/main/java/hello/servlet/web/frontcontroller/v3/ControllerV3.java
+++ src/main/java/hello/servlet/web/frontcontroller/v3/ControllerV3.java
@@ -1,16 +1,10 @@
 package hello.servlet.web.frontcontroller.v3;
 
-import hello.servlet.web.frontcontroller.v2.MyView;
-import hello.servlet.web.frontcontroller.v3.ModelView;
+import hello.servlet.web.frontcontroller.ModelView;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Map;
 
 public interface ControllerV3 {
 
-    ModelView process(Map<String,Object> paramMap) throws ServletException, IOException;
-
+    ModelView process(Map<String, String> paramMap);
 }
--- src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberListControllerV3.java
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberListControllerV3.java
@@ -2,37 +2,22 @@ package hello.servlet.web.frontcontroller.v3.controller;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
-import hello.servlet.web.frontcontroller.v2.ControllerV2;
-import hello.servlet.web.frontcontroller.v2.MyView;
+import hello.servlet.web.frontcontroller.ModelView;
 import hello.servlet.web.frontcontroller.v3.ControllerV3;
-import hello.servlet.web.frontcontroller.v3.ModelView;
-import org.springframework.boot.Banner;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 public class MemberListControllerV3 implements ControllerV3 {
 
-    MemberRepository memberRepository = MemberRepository.getInstance();
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
 
-    // 회원 입력 등록 컨트롤러
     @Override
-    public ModelView process(Map<String, Object> paramMap) throws ServletException, IOException {
+    public ModelView process(Map<String, String> paramMap) {
         List<Member> members = memberRepository.findAll();
+        ModelView mv = new ModelView("members");
+        mv.getModel().put("members", members);
 
-        //request.setAttribute( "members", members );
-
-        //String viewPath = "/WEB-INF/views/members.jsp";
-        // //RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
-        // //dispatcher.forward(request, response);
-        //return new MyView(viewPath);
-
-        ModelView mv  = new ModelView("members");
-        mv.getModel().put("members",members);
         return mv;
     }
 }
